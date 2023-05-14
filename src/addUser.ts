c6988b8f7863d5b81ab4928bb3fcc6a71a567949
Moskwa Quentin moskwaquentin@gmail.com <95235691+QuentinMoskwa@users.noreply.github.com>
2023-05-14 23:31:45 +0200

    CRUD

---
+++ src/addUser.ts
@@ -0,0 +1,66 @@
+import mysql from "mysql";
+import bcrypt from "bcryptjs";
+import express from "express";
+
+const app = express();
+
+
+// Création de la connexion à la base de données
+const connection = mysql.createConnection({
+  host: "mysql-qmoskwa.alwaysdata.net",
+  user: "qmoskwa",
+  password: "23juin06",
+  database: "qmoskwa_tpfullstack",
+});
+
+// Test de la connexion à la base de données
+connection.connect((err) => {
+  if (err) {
+    console.error("Erreur de connexion à la base de données : ", err);
+  } else {
+    console.log("Connexion à la base de données établie.");
+  }
+});
+
+// Fermeture de la connexion à la base de données lorsque l'application est arrêtée
+process.on("SIGINT", () =>
+  connection.end((err) => {
+    console.log("Fermeture de la connexion à la base de données.");
+    process.exit(err ? 1 : 0);
+  })
+);
+
+
+const addUser = (login: string, password: string, token: string) => {
+  const saltRounds = 10;
+  const hashedPassword = bcrypt.hashSync(password, saltRounds);
+  //   verification de l'unicité du nouveau nom d'utilisateur
+  connection.query(
+    "SELECT COUNT(*) AS count FROM userTab WHERE login = ?",
+    [login],
+    (error, results) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+
+      const count = results[0].count;
+      if (count > 0) {
+        console.log(`User ${login} already exists.`);
+        return;
+      }
+    //   insertion du nouvel utilisateur
+      const user = { login, password: hashedPassword, token };
+      connection.query("INSERT INTO userTab SET ?", user, (error, results) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+        console.log(`User ${login} added successfully!`);
+      });
+    }
+  );
+};
+
+
+export default addUser;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import mysql from "mysql";
+import addUser from "./addUser";
+import removeUser from "./removeUser";
+import updateUser from "./updateUser";
 
 // Création de la connexion à la base de données
 const connection = mysql.createConnection({
@@ -18,19 +21,52 @@ connection.connect((err) => {
   }
 });
 
+function generateToken(): string {
+  const chars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const tokenLength = 20;
+  let token = "";
+  for (let i = 0; i < tokenLength; i++) {
+    token += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return token;
+}
+
+
+
+
+
 // Fermeture de la connexion à la base de données lorsque l'application est arrêtée
 process.on("SIGINT", () =>
-  connection.end((err) => {
+connection.end((err) => {
     console.log("Fermeture de la connexion à la base de données.");
     process.exit(err ? 1 : 0);
-  })
+})
 );
-
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    connection.query('SELECT * FROM userTab', (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Erreur lors de la récupération des utilisateurs');
+    } else {
+      res.send(results);
+    }
+  });
+});
+app.get("/addUser", (req, res) => {
+    addUser("test", "password123", generateToken());
+    res.redirect("/");
+});
+app.get("/removeUser", (req, res) => {
+    removeUser("test", "password123");
+    res.redirect("/");
+});
+app.get("/updateUser", (req, res) => {
+    updateUser("test","test2", "password123");
+    res.redirect("/");
 });
 
 const port = process.env.PORT || 3000;
